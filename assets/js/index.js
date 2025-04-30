77f398742dc05d5cec02fc142460c9a2aedf2e30
Luda Modna <ludmilamodna3@gmail.com>
2025-04-30 11:37:49 +0300

    Added task js dom + fetch/json

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,19 @@
 "use strict";
 
-//temperature_unit=fahrenheit
+const photoDogUrl = "https://dog.ceo/api/breeds/image/random";
+const photoDog = document.createElement("img");
+document.body.append(photoDog);
 
-let isCelsiusDegrii = true;
-
-const tempUnitBtn = document.getElementById("tempUnitBtn");
-
-
-tempUnitBtn.onclick = switchTemperatureUnit;
-function switchTemperatureUnit() {
-  isCelsiusDegrii = !isCelsiusDegrii;
-  updateData();
+function updatePhotoDog({ message }) {
+  photoDog.src = message;
+  photoDog.classList.add("photoDog");
 }
 
-updateData();
-
-function updateData() {
-  tempUnitBtn.textContent = `Switch to ${isCelsiusDegrii ? "F" : "C"}`;
-
-  const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current_weather=true&timezone=auto${
-    isCelsiusDegrii ? "" : "&temperature_unit=fahrenheit"
-  }`;
-
-  fetch(weatherUrl)
+function randomPhotoBtn() {
+  fetch(photoDogUrl)
     .then((response) => response.json())
-    .then((data) => updateWeather(data))
+    .then((data) => updatePhotoDog(data))
     .catch((err) => console.log(err));
 }
 
-function updateWeather({
-  current_weather: { temperature, windspeed },
-  current_weather_units: { temperature: tempUnit, windspeed: windspeedUnit },
-}) {
-  const currentemperatureEl = document.querySelector(".temp");
-  currentemperatureEl.textContent = `${temperature} ${tempUnit}`;
-  currentemperatureEl.style.color = calcColorTemperature(temperature);
-  currentemperatureEl.classList.add("weather");
-
-  const currentWindEl = document.querySelector(".wind");
-  currentWindEl.textContent = `${windspeed} ${windspeedUnit}`;
-  currentWindEl.classList.add("weather");
-}
-
-function calcColorTemperature(temperature) {
-  if (temperature < 0) {
-    return "blue";
-  } else if (temperature === 0) {
-    return "black";
-  } else if (temperature > 0 && temperature < 40) {
-    return "green";
-  } else {
-    return "red";
-  }
-}
+document.querySelector("button").addEventListener("click", randomPhotoBtn);
